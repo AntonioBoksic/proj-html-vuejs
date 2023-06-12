c7867ff0131d8cddfc547602bcff5474731d3aaa
Antonio <antonioboksic1603@gmail.com>
2023-06-12 18:05:14 +0200

    header quasi finito, fatto in maniera statica per ora, devo solo capire perchè non riesco a gestire bene le icone prima di passare a parte dinamica

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -26,13 +26,124 @@ export default {
 <!-- HTML -->
 <template>
   
-  <h1>PageHeader</h1>
+    <header>
+        <div class="container">
+            <div class="logo-header">
+                <img src="../../public/img/logo-2.png" alt="">
+            </div>
+
+            <ul>
+                <li>
+                    <a href="#">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        About us
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Feature
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Testimonials
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Contact us
+                    </a>
+                </li>
+
+                <li class="list-item-icone">
+                    <div>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                    
+                    <div>
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    
+                    </div>
+                </li>
+
+                <li>
+                    
+                        <button>Get Started</button>
+                    
+                    
+                </li>
+
+            </ul>
+
+        </div>
+
+    </header>
 
 </template>
 
 <!-- STILE -->
 
 <style scoped lang="scss">
+
+header {
+    height: 80px;
+    border: black 1px solid;
+    
+
+    .container{
+        height: 100%;
+        width: 75%;
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+
+        .logo-header {
+        height: 50px;
+        width: 50px;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    ul {
+        display: flex;
+        align-items: center;
+        
+        li {
+            padding: 0px 10px;
+
+            .list-item-icone {
+                display: flex;
+
+                i {
+                    padding: 0px 10px;
+                }
+            }
+
+            
+
+        }
+
+        button {
+            background-color: #377dff;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            padding: 10px;
+        }
+    }
+    }
+
+    
+}
 
 
 </style>
